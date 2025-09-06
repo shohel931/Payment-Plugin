421fc6dcef789a2a0910961161c24afc29643e72
MD. Shohel Rana <shohel065931@gmail.com>
2025-09-06 20:55:26 +0600

    f

--- includes/easypay-prossesing.php
+++ includes/easypay-prossesing.php
@@ -4,25 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/easypay-style.css">
-    <title>EasyPay - Prossesing</title>
+    <title>Payment Successful!</title>
 </head>
 <body>
     
 <section id="easypay_main_area">
     <div class="easypay_sub_area">
-        <div class="easypay_text">
+        <div class="easypay_text" style="margin-bottom: 10px;">
         <h2>Payment Successful!</h2>
         </div>
-        <div class="easypay_box">
+        <div class="easypay_box" style="text-align: center; border: none; background: #fff;">
             <div class="easypay_content">
-                <h5>Order ID : <span>#<?php echo $order ? $order->get_id() : ''; ?></span></h5>
+                <img src="<?php echo plugins_url('/assets/success.png', __FILE__); ?>" alt="success" style="width: 100px; height: 100px; margin-bottom: 10px;">
             </div>
             <div class="easypay_content">
-                <input type="button" onclick="window.location.href='<?php echo home_url(); ?>';" value="Back Home">
+                <input type="submit"  value="Back Home">
             </div>
         </div>
     </div>
 </section>
+
+
+
+
+
 
 
 </body>
